39afd524e7db37abb6d183c09c9cb64e7cf894b6
Alperen Özkaya <ozkayaal@mef.edu.tr>
2024-02-20 00:43:27 +0300

    Update face_detection_train.py

--- face_detection_train.py
+++ face_detection_train.py
@@ -1,6 +1,5 @@
 from ultralytics import YOLO
 
-<<<<<<< HEAD
 
 def main():
     model = YOLO('yolov8n.pt')
@@ -11,10 +10,9 @@ def main():
 
 if __name__ == '__main__':
     main()
-=======
+
 # Load a model
-model = YOLO('yolov8n.pt')  # load a pretrained model (recommended for training)
+model = YOLO('yolov8n.pt')
 
 # Train the model
 results = model.train(data='coco128.yaml', epochs=100, imgsz=640)
->>>>>>> 6fbf35bab9927c046499f86ecf5790e25fe87d4f
